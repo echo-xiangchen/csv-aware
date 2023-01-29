a76ba16983b80bdb007fe5a78c7128c4e215fe8f
Echo <echo.xiangchen@gmail.com>
2023-01-28 21:42:26 -0500

    update function to compare neo4j and souffle

--- src/expr/root/CapStarEndNodeFromNeo4jResult.java
+++ src/expr/root/CapStarEndNodeFromNeo4jResult.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class CapStarEndNodeFromNeo4jResult {
 	public static void main(String[] args) {
-		if (args.length != 1) {
+		if (args.length < 2 ) {
 			System.out.println("Usage: "
 					+ "Input: a .csv file that contains analysis result from Neo4j.\n"
 					+ "Output: a .csv file that only contains start node and end node of paths.\n"
@@ -14,7 +14,8 @@ public class CapStarEndNodeFromNeo4jResult {
 			BufferedReader reader;
 			BufferedWriter writer;
 			
-			Map<String, List<String>> pathMap = new LinkedHashMap<>();
+			Map<String, HashSet<String>> neo4jMap = new LinkedHashMap<>();
+			Map<String, HashSet<String>> souffleMap = new LinkedHashMap<>();
 			
 			try {
 				reader = new BufferedReader(new FileReader(args[0]));
@@ -23,6 +24,8 @@ public class CapStarEndNodeFromNeo4jResult {
 				String line = reader.readLine();
 				
 				int linenum = 2;
+				
+				// first capture the start and end node of neo4j result
 				while ((line = reader.readLine()) != null) {
 					// some lines might be empty
 					if (line.equals("\"\"")) {
@@ -36,38 +39,80 @@ public class CapStarEndNodeFromNeo4jResult {
 					String endID = splitId[splitId.length - 1].split("\"\"")[0];
 					//System.out.println(line);
 					
-					if (!pathMap.containsKey(startID)) {
-						List<String> endList = new ArrayList<>();
-						endList.add(endID);
-						pathMap.put(startID, endList);
-					} else if (!pathMap.get(startID).contains(endID)) {
-						pathMap.get(startID).add(endID);
+					if (!neo4jMap.containsKey(startID)) {
+						HashSet<String> endIDset = new HashSet<>();
+						endIDset.add(endID);
+						neo4jMap.put(startID, endIDset);
+					} else if (!neo4jMap.get(startID).contains(endID)) {
+						neo4jMap.get(startID).add(endID);
 					}
-					System.out.println("finished line " + linenum);
+					System.out.println("finished capturing line " + linenum + " of neo4j result.");
 					linenum++;
 				}
 				
-				String output = "";
+				// then capture the start and end node of souffle result
+				reader = new BufferedReader(new FileReader(args[1]));
 				
-				// capture input file name, and use it in output file
-				String[] splitInputFileName = args[0].toString().split("/");
-				String filename = splitInputFileName[splitInputFileName.length - 1].split("\\.")[0];
+				String linesouffle;
 				
-				writer = new BufferedWriter(new FileWriter(filename + ".startEndID.csv"));
+				linenum = 1;
 				
-				System.out.println("writing " + filename + ".startEndID.csv");
-				for (Map.Entry<String, List<String>> entry : pathMap.entrySet()) {
-					String key = entry.getKey();
-					for (int i = 0; i < entry.getValue().size(); i++) {
-						output = output + key + "\t" + entry.getValue().get(i) + "\n";
-			        }
+				while ((linesouffle = reader.readLine()) != null) {
+					String[] splitLine = linesouffle.split("\t");
+					String startID = splitLine[0];
+					String endID = splitLine[1];
 					
+					// add start id and end id into souffleMap
+					if (!souffleMap.containsKey(startID)) {
+						HashSet<String> endIDset = new HashSet<>();
+						endIDset.add(endID);
+						souffleMap.put(startID, endIDset);
+					} else {
+						souffleMap.get(startID).add(endID);
+					}
+					System.out.println("finished capturing line " + linenum + " of souffle result.");
+					linenum++;
 				}
 				
-				// write to file
-				writer.write(output);
-				writer.close();
-				System.out.println("writing " + filename + ".startEndID.csv finished.");
+				if (neo4jMap.keySet().equals(souffleMap.keySet())) {
+					System.out.println("keys are equal");
+				} else {
+					System.out.println("keys are not equal");
+				}
+				
+				String keyValue = "";
+				for (Map.Entry<String, HashSet<String>> entry : neo4jMap.entrySet()) {
+					String key = entry.getKey();
+					if (!neo4jMap.get(key).equals(souffleMap.get(key))) {
+						keyValue = keyValue + "key-value pair for " + key + "is not equal.";
+					}
+				}
+				
+				if (keyValue.isBlank()) {
+					System.out.println("key-value pairs of neo4j and souffle are identical.");
+				}
+				
+//				String output = "";
+				
+//				// capture input file name, and use it in output file
+//				String[] splitInputFileName = args[0].toString().split("/");
+//				String filename = splitInputFileName[splitInputFileName.length - 1].split("\\.")[0];
+//				
+//				writer = new BufferedWriter(new FileWriter(filename + ".startEndID.csv"));
+//				
+//				System.out.println("writing " + filename + ".startEndID.csv");
+//				for (Map.Entry<String, List<String>> entry : neo4jMap.entrySet()) {
+//					String key = entry.getKey();
+//					for (int i = 0; i < entry.getValue().size(); i++) {
+//						output = output + key + "\t" + entry.getValue().get(i) + "\n";
+//			        }
+//					
+//				}
+//				
+//				// write to file
+//				writer.write(output);
+//				writer.close();
+//				System.out.println("writing " + filename + ".startEndID.csv finished.");
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
--- src/expr/root/CompareNeo4jSouffle.java
+++ src/expr/root/CompareNeo4jSouffle.java
@@ -1,127 +1,193 @@
 package expr.root;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
+import java.io.*;
+import java.util.*;
 
 public class CompareNeo4jSouffle {
 	public static void main(String[] args) {
-		
-		if (args.length != 2) {
+		if (args.length < 2 ) {
 			System.out.println("Usage: "
-					+ "Input: two .csv files that contains analysis result from Neo4j and souffle.\n"
-					+ "Output: a .csv file that contains the difference between the results.\n"
+					+ "Input: two .csv file that contains analysis result from Neo4j and souffle.\n"
+					+ "Output: if results from Neo4j and souffle are identical.\n"
 					+ "Warning: You do not include any .csv files.");
 		} else {
 			BufferedReader reader;
 			BufferedWriter writer;
 			
+			Map<String, HashSet<String>> neo4jMap = new LinkedHashMap<>();
+			Map<String, HashSet<String>> souffleMap = new LinkedHashMap<>();
+			Map<String, HashSet<String>> inNeo4jNotSouffleMap = new LinkedHashMap<>();
+			Map<String, HashSet<String>> inSouffleNotNeo4jMap = new LinkedHashMap<>();
+			
 			try {
-				// args[0] is result from souffle
-			    reader = new BufferedReader(new FileReader(args[0]));
-			    
-				Map<String, List<String>> souffleMap = new LinkedHashMap<>();
-			    
-			    String line;
-			    
-			    while ((line = reader.readLine()) != null) {
-					// get and split string
-			    	String[] splitId = line.trim().split("\t");
-			    	String startID = splitId[0];
-			    	String endID = splitId[1];
-			    		
-			    	if (!souffleMap.containsKey(startID)) {
-			    		List<String> endList = new ArrayList<>();
-						endList.add(endID);
-						souffleMap.put(startID, endList);
-					} else if (!souffleMap.get(startID).contains(endID)) {
-						souffleMap.get(startID).add(endID);
-					}
-				}
-			    
-			    // args[1] is result from neo4j
-			    reader = new BufferedReader(new FileReader(args[1]));
-			    
-			    Map<String, List<String>> neo4jMap = new LinkedHashMap<>();
-			    
-			    // skip the first line - it's the title of Neo4j's result
-				line = reader.readLine();
+				// args[0] is neo4j output
+				reader = new BufferedReader(new FileReader(args[0]));
 				
+				// skip the first line - it's the title of Neo4j's result
+				String line = reader.readLine();
+				
+				int linenum = 2;
+				
+				// first capture the start and end node of neo4j result
 				while ((line = reader.readLine()) != null) {
-					String[] splitId = line.trim().split("\t");
-					String startID = splitId[0];
-					String endID = splitId[splitId.length - 1];
+					// some lines might be empty
+					if (line.equals("\"\"")) {
+						// dont use break here! it will quit the while loop!
+						// use continue.
+						//break;
+						continue;
+					}
+					String[] splitId = line.trim().split("id\"\":\"\"");
+					String startID = splitId[1].split("\"\"")[0];
+					String endID = splitId[splitId.length - 1].split("\"\"")[0];
 					//System.out.println(line);
 					
 					if (!neo4jMap.containsKey(startID)) {
-						List<String> endList = new ArrayList<>();
-						endList.add(endID);
-						neo4jMap.put(startID, endList);
+						HashSet<String> endIDset = new HashSet<>();
+						endIDset.add(endID);
+						neo4jMap.put(startID, endIDset);
 					} else if (!neo4jMap.get(startID).contains(endID)) {
 						neo4jMap.get(startID).add(endID);
 					}
+					System.out.println("finished capturing line " + linenum + " of neo4j result.");
+					linenum++;
 				}
 				
-				for (Map.Entry<String, List<String>> souffleentry : souffleMap.entrySet()) {
-					String soufflekey = souffleentry.getKey();
+				// then capture the start and end node of souffle result
+				reader = new BufferedReader(new FileReader(args[1]));
+				
+				String linesouffle;
+				
+				linenum = 1;
+				
+				while ((linesouffle = reader.readLine()) != null) {
+					String[] splitLine = linesouffle.split("\t");
+					String startID = splitLine[0];
+					String endID = splitLine[1];
 					
-					if (!neo4jMap.containsKey(soufflekey)) {
-						System.out.println("key not in neo4j: " + soufflekey);
+					// add start id and end id into souffleMap
+					if (!souffleMap.containsKey(startID)) {
+						HashSet<String> endIDset = new HashSet<>();
+						endIDset.add(endID);
+						souffleMap.put(startID, endIDset);
 					} else {
+						souffleMap.get(startID).add(endID);
+					}
+					System.out.println("finished capturing line " + linenum + " of souffle result.");
+					linenum++;
+				}
+				
+				// output the start and end ID of neo4j result
+				String output = "";
+				
+				// capture input file name, and use it in output file
+				String[] splitInputFileName = args[0].toString().split("/");
+				String filename = splitInputFileName[splitInputFileName.length - 1].split("\\.")[0];
+				
+				writer = new BufferedWriter(new FileWriter(filename + ".startEndID.csv"));
+				
+				System.out.println("writing " + filename + ".startEndID.csv");
+				for (Map.Entry<String, HashSet<String>> entry : neo4jMap.entrySet()) {
+					String key = entry.getKey();
+					for (String value : neo4jMap.get(key)) {
+						output = output + key + "\t" + value + "\n";
+			        }
+					
+				}
+				
+				// write to file
+				writer.write(output);
+				writer.close();
+				System.out.println("writing " + filename + ".startEndID.csv finished.");
+				
+				
+				if (neo4jMap.keySet().equals(souffleMap.keySet())) {
+					System.out.println("keys are equal");
+				} else {
+					System.out.println("keys are not equal");
+				}
+				
+				for (Map.Entry<String, HashSet<String>> entry : neo4jMap.entrySet()) {
+					String key = entry.getKey();
+					
+					if (!neo4jMap.get(key).equals(souffleMap.get(key))) {
+						// first copy two hashsets (value representing end IDs)
+						HashSet<String> inNeo4jNotSouffle = new HashSet<>(neo4jMap.get(key));
+						HashSet<String> inSouffleNotNeo4j = new HashSet<>(souffleMap.get(key));
 						
-						for (int i = 0; i < souffleentry.getValue().size(); i++) {
-							if (!neo4jMap.get(soufflekey).contains(souffleentry.getValue().get(i))) {
-								System.out.println("key-value pair not in neo4j: " 
-										+ soufflekey + "\t" + souffleentry.getValue().get(i));
-							}
-				        }
+						// captures the intersection of neo4j and souffle
+						HashSet<String> intersection = new HashSet<>(neo4jMap.get(key));
+						intersection.retainAll(souffleMap.get(key));
+						
+						// Neo4j - intersection is the end IDs that in Neo4j but not in souffle
+						inNeo4jNotSouffle.removeAll(intersection);
+						
+						// souffle - intersection is the end ID that in souffle but not in neo4j
+						inSouffleNotNeo4j.removeAll(intersection);
+						
+						// capture end IDs returned by neo4j but not by souffle
+						if (!inNeo4jNotSouffle.isEmpty()) {
+							inNeo4jNotSouffleMap.put(key, inNeo4jNotSouffle);
+						}
+						
+						// capture end IDs returned by souffle but not by neo4j
+						if (!inSouffleNotNeo4j.isEmpty()) {
+							inSouffleNotNeo4jMap.put(key, inSouffleNotNeo4j);
+						}
+						
 					}
 				}
-			    
-			    
-			    
-//			    String output = "";
-//				
-//				// capture input file name, and use it in output file
-//				String[] splitInputFileName = args[0].toString().split("/");
-//				String filename = splitInputFileName[splitInputFileName.length - 1].split("\\.")[0];
-//				
-//				writer = new BufferedWriter(new FileWriter(filename + ".startEndID.csv"));
-//				
-//				System.out.println("writing " + filename + ".startEndID.csv");
-//				for (Map.Entry<String, List<String>> entry : souffleMap.entrySet()) {
-//					String key = entry.getKey();
-//					for (int i = 0; i < entry.getValue().size(); i++) {
-//						output = output + key + "\t" + entry.getValue().get(i) + "\n";
-//			        }
-//					
-//				}
-//				
-//				// write to file
-//				writer.write(output);
-//				writer.close();
-//				System.out.println("writing " + filename + ".startEndID.csv finished.");
-			    
-	    
-//			    int total = 0;
-//			    for (Map.Entry<String, List<String>> entry : pathMap.entrySet()) {
-//					String key = entry.getKey();
-//					total = total + pathMap.get(key).size();
-//				}
-//			    
-//			    System.out.println(total);
 				
+				// if there are any key-value pair return by neo4j but not souffle
+				if (!inNeo4jNotSouffleMap.isEmpty()) {
+					String writeInNeo4jNotSouffle = "";
+					
+					writer = new BufferedWriter(new FileWriter(filename + ".inNeo4jNotSouffle.csv"));
+					
+					System.out.println("writing " + filename + ".inNeo4jNotSouffle.csv");
+					for (Map.Entry<String, HashSet<String>> entry : inNeo4jNotSouffleMap.entrySet()) {
+						String key = entry.getKey();
+						for (String value : inNeo4jNotSouffleMap.get(key)) {
+							writeInNeo4jNotSouffle = writeInNeo4jNotSouffle + key + "\t" + value + "\n";
+				        }
+						
+					}
+					
+					// write to file
+					writer.write(writeInNeo4jNotSouffle);
+					writer.close();
+					System.out.println("writing " + filename + ".inNeo4jNotSouffle.csv finished.");
+				} 
+				
+				// if there are any key-value pair return by souffle but not neo4j
+				if (!inSouffleNotNeo4jMap.isEmpty()) {
+					String writeInSouffleNotNeo4j = "";
+					
+					writer = new BufferedWriter(new FileWriter(filename + ".inSouffleNotNeo4j.csv"));
+					
+					System.out.println("writing " + filename + ".inSouffleNotNeo4j.csv");
+					for (Map.Entry<String, HashSet<String>> entry : inSouffleNotNeo4jMap.entrySet()) {
+						String key = entry.getKey();
+						for (String value : inSouffleNotNeo4jMap.get(key)) {
+							writeInSouffleNotNeo4j = writeInSouffleNotNeo4j + key + "\t" + value + "\n";
+				        }
+						
+					}
+					
+					// write to file
+					writer.write(writeInSouffleNotNeo4j);
+					writer.close();
+					System.out.println("writing " + filename + ".inSouffleNotNeo4j.csv finished.");
+				}
+				
+				// if both inNeo4jNotSouffleMap and inSouffleNotNeo4jMap are empty
+				// then results are identical
+				if (inNeo4jNotSouffleMap.isEmpty() && inSouffleNotNeo4jMap.isEmpty()) {
+					System.out.println("Results of Neo4j and Souffle are identical.");
+				}
 			} catch (Exception e) {
 				e.printStackTrace();
-			} 
+			}
 		}
 	}
 }
